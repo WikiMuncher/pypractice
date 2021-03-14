8ab51335ce21f3c75c1d8f2bc9af21af5e2b1682
WikiMuncher <woo0421!>
2021-03-14 16:51:13 +0900

    added translate test

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 word = "Neat"
 words = "Simple name"
 
-say = "henlo {}, let me {}"
-ws = say.format("icecream", "lick you up")
+say = "henlo {name}, let me {line}, {name}{name}"
+ws = say.format(name="icecream", line="lick you up")
 
 print(ws + " plz ")
+
+# maketrans test
+temp = ws.maketrans("elo", "zxq")
+
+print(temp, ws.translate(temp))
